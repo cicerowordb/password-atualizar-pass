fc94b08dc3e7cb2650a8839783b95c7fba437498
Cicero Woshington <cicerow.ordb@gmail.com>
2022-11-27 11:41:44 -0300

    changes to update frontend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """ ATUALIZAR-DATA-BASE: permite trocar uma senha na lista.
 Principal Referência: https://docs.microsoft.com/pt-br/azure/mysql/connect-python
 apt install python3 python3-pip
-pip3 install flask mysql-connector-python """
+pip3 install flask mysql-connector-python"""
 
 import os
 from flask import Flask
@@ -21,15 +21,6 @@ mysql_database = os.getenv('MYSQL_DATABASE')
 pontuacao = os.getenv('PONTUACAO') #'@#$%()[]<>'
 
 app = Flask(__name__)
-output1 = '''<h1>ATUALIZAR</h1>
-Atualiza a senha em um serviço.
-/atualizar/<servico>/<user>/<password>
-'''
-
-@app.route('/')
-def root_server():
-    """ Server Root """
-    return output1, 200
 
 @app.route('/atualizar/<servico>/<user>/<password>')
 def atualizar(servico='', user='', password=''):
@@ -40,21 +31,21 @@ def atualizar(servico='', user='', password=''):
         'password':mysql_password,
         'database':mysql_database
     }
-    result = "ERRO", 500
+    result = [False, "ERRO", 500]
     try:
         conn = mysql.connector.connect(**config)
         print("Conexao bem sucedida")
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("ERRO: Nome ou senha do banco incorretas (permissao)")
-            result = "ERRO: Nome ou senha do banco incorretas (permissao)", 501
+            result = [False, "ERRO: Nome ou senha do banco incorretas (permissao)", 501]
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("ERRO: Banco não existe no servidor consultado")
-            result = "ERRO: Banco nao existe no servidor consultado", 502
+            result = [False, "ERRO: Banco nao existe no servidor consultado", 502]
         else:
             print("ERRO: não identificado no codigo")
             print(err)
-            result = "ERRO: não identificado no codigo", 503
+            result = [False, "ERRO: não identificado no codigo", 502]
     else:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM dados_logon WHERE user='"+user+"' AND servico='"+servico+"';")
@@ -63,12 +54,12 @@ def atualizar(servico='', user='', password=''):
         if cursor.rowcount > 0:
             cursor.execute("UPDATE dados_logon SET password='" + password + \
                            "' WHERE user='" + user + "' AND servico='" + servico + "';")
-            result = "Registro localizado\n"
+            result = [True, 'Registro atualizado.', 200]
             print("Registro localizado")
         else:
-            result = "Nenhum registro encontrado\n"
+            result = [False, "Nenhum registro encontrado.", 500]
             print("Registro não localizado")
-        print("Executando o commit")
+        print("Executando o commit.")
         conn.commit()
         cursor.close()
         conn.close()
